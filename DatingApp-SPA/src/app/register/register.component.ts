af8d15e25a64cff3f44e791c516b1dacc0d16485
Mikhail Kiyan <Kiyan@ukr.net>
2019-03-14 18:39:39 +0200

    Third libraries have been provided (alertifyjs, angular-jwt, ngx-bootstrap, bootswatch)

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,28 +12,23 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService) {
-
-  }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() { }
 
   register() {
     console.log(this.model);
-    this.authService
-      .register(this.model)
-      .subscribe(
-        next => {
-          console.log('Registration successful');
+    this.authService.register(this.model).subscribe(
+      () => {
+        this.alertify.success('Registration successful');
         },
-        error => {
-          console.log(error);
+      error => {
+        this.alertify.error(error);
         }
       );
   }
 
   cancel() {
-    console.log('cencelled');
     this.cancelRegister.emit(false);
   }
 }
